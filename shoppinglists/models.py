69d51a866cd647f6817e86dfd1ffd763e34c9f9b
Joyce Namuli <joyce.namuli@andela.com>
2018-07-17 15:48:26 +0300

    Add coveragein config

--- shoppinglists/models.py
+++ shoppinglists/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 # Create your models here.
 # from django.db import models
-# import django
-# django.setup()
 
 class Shoppinglist(models.Model):
     name = models.CharField(max_length=100)
